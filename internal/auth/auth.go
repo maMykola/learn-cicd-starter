f45461e862dec0c442acdaeed78dc2d854a82805
Mykola Martynov <mykola.martynov@hotmail.com>
2026-09-18 22:24:56 +0300

    remove broken code

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -10,11 +10,6 @@ var ErrNoAuthHeaderIncluded = errors.New("no authorization header included")
 
 // GetAPIKey -
 func GetAPIKey(headers http.Header) (string, error) {
-	// temporarily break the code to verify tests in ci/cd
-	if true {
-		return "", ErrNoAuthHeaderIncluded
-	}
-
 	authHeader := headers.Get("Authorization")
 	if authHeader == "" {
 		return "", ErrNoAuthHeaderIncluded
